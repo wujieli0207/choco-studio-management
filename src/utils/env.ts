04a2c49cd2adcdfde812cba1ba7bc6c3770daca1
wujieli0207 <lwj0207@foxmail.com>
2022-03-08 15:50:10 +0800

    feat: 增加获取环境变量函数

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -29,3 +29,15 @@ export function getAppEnvConfig() {
     VITE_GLOB_UPLOAD_URL,
   };
 }
+
+export function getEnv(): string {
+  return import.meta.env.MODE;
+}
+
+export function isDevMode(): boolean {
+  return import.meta.env.DEV;
+}
+
+export function isProdMode(): boolean {
+  return import.meta.env.PROD;
+}
