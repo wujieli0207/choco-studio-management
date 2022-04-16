89cfba97ded287226587feec3e21a655d175f60d
wujieli0207 <wujieli0207@outlook.com>
2022-04-16 22:51:43 +0800

    style: 文字内容调整

--- src/views/article/articleManage.vue
+++ src/views/article/articleManage.vue
@@ -1,7 +1,7 @@
 <template>
   <el-card>
     <el-tabs v-model="activeName">
-      <el-tab-pane label="文章列表" name="allArticle">allArticle</el-tab-pane>
+      <el-tab-pane label="文章列表" name="allArticle">articleList</el-tab-pane>
       <el-tab-pane label="草稿箱" name="draft">draft</el-tab-pane>
     </el-tabs>
   </el-card>
